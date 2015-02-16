6b68ea0c2dd220739844eb637ba22f52fb0a6939
adeelsyed <adeel.syed@gmail.com>
2015-02-16 14:31:44 -0800

    order by breakthrough POC, woohoo!

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,50 +10,21 @@ namespace ConsoleApp
 {
 	static class Program
 	{
+		static private readonly StringBuilder XmlBuilder = new StringBuilder();
 
 		static void Main()
 		{
-			TSqlFragment sqlFragment = ParseSql("..//..//testsql.sql");
+			TSqlFragment tree = GetTSqlFragmentTree("..//..//testsql.sql");
 
-            //default formatting. can take an SqlScriptGeneratorOptions argument for very basic formatting options
-            Console.WriteLine("--DEFAULT FORMATTING\n");
-            GenerateScript(sqlFragment, null);
-
-            //token iteration
-            foreach (var fragment in sqlFragment.ScriptTokenStream.Where(t => t.TokenType != TSqlTokenType.WhiteSpace))
-            {
-                Console.Write(fragment.Text);
-                Console.SetCursorPosition(20, Console.CursorTop);
-                Console.Write(fragment.TokenType);
-                Console.SetCursorPosition(40, Console.CursorTop);
-                Console.WriteLine(fragment.IsKeyword());
-            }
-
-            foreach (var t in sqlFragment.ScriptTokenStream.Where(t => t.TokenType != TSqlTokenType.WhiteSpace))
-            {
-                if (t.IsKeyword())
-                    Console.WriteLine();
-
-                Console.Write(t.Text);
-
-                if (t.TokenType == TSqlTokenType.Comma || t.IsKeyword())
-                    Console.WriteLine();
-            }
-
-            //xml visualizer
-            StringBuilder result = new StringBuilder();
-            ScriptDomWalk(sqlFragment, "root", result);
-            Console.Write(result);
-
-            //visitor pattern
-            sqlFragment.Accept(new PutEachStatementOnItsOwnLine());
-            sqlFragment.Accept(new PutEachClauseOnItsOwnLine());
-
+            //DefaultFormatter(SqlFragment, null);
+			//XmlVisualiser(SqlFragment);
+			//VisitorPattern(SqlFragment);
+			Beta(tree);
+			
 			Console.ReadLine();
 		}
 
-        //TODO: move to shared library
-		private static TSqlFragment ParseSql(string path)
+		private static TSqlFragment GetTSqlFragmentTree(string path)
 		{
 			using (TextReader txtRdr = new StreamReader(path))
 			{
@@ -71,118 +42,154 @@ namespace ConsoleApp
 				return sqlFragment;
 			}
 		}
-
-        private static void GenerateScript(TSqlFragment sqlFragment, SqlScriptGeneratorOptions options = null)
+        private static void DefaultFormatter(TSqlFragment sqlFragment, SqlScriptGeneratorOptions options = null)
         {
-            SqlScriptGenerator generator = options == null ? new Sql110ScriptGenerator() : new Sql110ScriptGenerator(options);
+			var generator = new Sql110ScriptGenerator(options ?? new SqlScriptGeneratorOptions());
 
-            using (TextWriter consoleWriter = new StreamWriter(Console.OpenStandardOutput()))
-            {
+			using (var consoleWriter = new StreamWriter(Console.OpenStandardOutput()))
                 generator.GenerateScript(sqlFragment, consoleWriter);
-            }
         }
+		private static void XmlVisualiser(TSqlFragment sqlFragment)
+		{
+			XmlVisualizerScriptDomWalk(sqlFragment, "root");
+			using(var writer = new StreamWriter(File.Create("..//..//XmlVisualiser.xml")))
+				writer.Write(XmlBuilder);
+		}
+		private static void XmlVisualizerScriptDomWalk(object fragment, string memberName)
+		{
+			if (fragment.GetType().BaseType.Name != "Enum")
+			{
+				XmlBuilder.AppendLine("<" + fragment.GetType().Name + " memberName = '" + memberName + "'>");
+			}
+			else
+			{
+				XmlBuilder.AppendLine("<" + fragment.GetType().Name + "." + fragment.ToString() + "/>");
+				return;
+			}
 
-        private static void ScriptDomWalk(object fragment, string memberName, StringBuilder result)
-        {
-            if (fragment.GetType().BaseType.Name != "Enum")
-            {
-                result.AppendLine("<" + fragment.GetType().Name + " memberName = '" + memberName + "'>");
-            }
-            else
-            {
-                result.AppendLine("<" + fragment.GetType().Name + "." + fragment.ToString() + "/>");
-                return;
-            }
+			Type t = fragment.GetType();
 
-            Type t = fragment.GetType();
+			PropertyInfo[] pibase;
+			if (null == t.BaseType)
+			{
+				pibase = null;
+			}
+			else
+			{
+				pibase = t.BaseType.GetProperties();
+			}
 
-            PropertyInfo[] pibase;
-            if (null == t.BaseType)
-            {
-                pibase = null;
-            }
-            else
-            {
-                pibase = t.BaseType.GetProperties();
-            }
+			foreach (PropertyInfo pi in t.GetProperties())
+			{
+				if (pi.GetIndexParameters().Length != 0)
+				{
+					continue;
+				}
 
-            foreach (PropertyInfo pi in t.GetProperties())
-            {
-                List<string> ignore = new List<string>() { "StartOffset", "FragmentLength", "StartLine", "StartColumn", "FirstTokenIndex", "LastTokenIndex" };
-                if (pi.GetIndexParameters().Length != 0 || ignore.Contains(pi.Name))
-                {
-                    continue;
-                }
+				if (pi.PropertyType.BaseType != null)
+				{
+					if (pi.PropertyType.BaseType.Name == "ValueType")
+					{
+						XmlBuilder.Append("<" + pi.Name + ">" + pi.GetValue(fragment, null).ToString() + "</" + pi.Name + ">");
+						continue;
+					}
+				}
 
-                if (pi.PropertyType.BaseType != null)
-                {
-                    if (pi.PropertyType.BaseType.Name == "ValueType")
-                    {
-                        result.Append("<" + pi.Name + ">" + pi.GetValue(fragment, null).ToString() + "</" + pi.Name + ">");
-                        continue;
-                    }
-                }
+				if (pi.PropertyType.Name.Contains(@"IList`1"))
+				{
+					if ("ScriptTokenStream" != pi.Name)
+					{
+						var listMembers = pi.GetValue(fragment, null) as IEnumerable<object>;
 
-                if (pi.PropertyType.Name.Contains(@"IList`1"))
-                {
-                    if ("ScriptTokenStream" != pi.Name)
-                    {
-                        var listMembers = pi.GetValue(fragment, null) as IEnumerable<object>;
+						foreach (object listItem in listMembers)
+						{
+							XmlVisualizerScriptDomWalk(listItem, pi.Name);
+						}
+					}
+				}
+				else
+				{
+					object childObj = pi.GetValue(fragment, null);
 
-                        foreach (object listItem in listMembers)
-                        {
-                            ScriptDomWalk(listItem, pi.Name, result);
-                        }
-                    }
-                }
-                else
-                {
-                    object childObj = pi.GetValue(fragment, null);
+					if (childObj != null)
+					{
+						if (childObj.GetType() == typeof(string))
+						{
+							XmlBuilder.Append(pi.GetValue(fragment, null));
+						}
+						else
+						{
+							XmlVisualizerScriptDomWalk(childObj, pi.Name);
+						}
+					}
+				}
+			}
 
-                    if (childObj != null)
-                    {
-                        if (childObj.GetType() == typeof(string))
-                        {
-                            result.Append(pi.GetValue(fragment, null));
-                        }
-                        else
-                        {
-                            ScriptDomWalk(childObj, pi.Name, result);
-                        }
-                    }
-                }
-            }
+			XmlBuilder.AppendLine("</" + fragment.GetType().Name + ">");
+		}
+		private static void VisitorPattern(TSqlFragment sqlFragment)
+		{
+			sqlFragment.Accept(new BatchVisitor());
+			Console.WriteLine("-------------------");
+			sqlFragment.Accept(new StatementVisitor());
+		}
+		private static void Beta(TSqlFragment sqlFragment)
+		{
+			//format order by
+			sqlFragment.Accept(new OrderByVisitor());
 
-            result.AppendLine("</" + fragment.GetType().Name + ">");
-        }
-
+			Console.Write(sqlFragment.ToText());
+		}
 	}
 
-    internal class PutEachStatementOnItsOwnLine : TSqlFragmentVisitor
-    {
-        public override void Visit(TSqlStatement node)
-        {
-            Console.WriteLine(node.ToText());
-            Console.WriteLine();
-        }
-    }
-    internal class PutEachClauseOnItsOwnLine : TSqlFragmentVisitor
-    {
-        public override void Visit(TSqlFragment fragment)
-        {
-            Console.WriteLine(fragment.ToText());
-        }
+	internal class BatchVisitor : TSqlFragmentVisitor
+	{
+		public override void ExplicitVisit(TSqlBatch batch)
+		{
+			Console.WriteLine(batch.ToText());
+			Console.WriteLine("GO");
+		}
+	}
+	internal class StatementVisitor : TSqlFragmentVisitor
+	{
+		public override void Visit(TSqlStatement statement)
+		{
+			Console.WriteLine(statement.ToText());
+			Console.WriteLine();
+		}
+	}
+	internal class OrderByVisitor : TSqlFragmentVisitor
+	{
+		public override void Visit(OrderByClause f)
+		{
+			//get tokens
+			var sb = new StringBuilder();
+			for (int i = f.FirstTokenIndex; i <= f.LastTokenIndex; i++)
+			{
+				var t = f.ScriptTokenStream[i];
+				switch (t.TokenType)
+				{
+					case TSqlTokenType.Order:
+						t.Text = "\nORDER BY\n";
+						break;
+					case TSqlTokenType.Identifier:
+						t.Text = "\t" + t.Text;
+						break;
+					case TSqlTokenType.Comma:
+						t.Text = ",\n";
+						break;
+					case TSqlTokenType.By:
+					case TSqlTokenType.WhiteSpace:
+						t.Text = "";
+						break;
+					default:
+						sb.Append(t.Text);
+						break;
+				}
 
-        public override void Visit(TSqlBatch batch)
-        {
-            foreach(var statement in batch.Statements)
-            {
-                Console.WriteLine(statement.ToText());
-            }
-        }
-    }
-
-    //TODO: move to shared library
+			}
+		}
+	}
 	internal static class Extensions
 	{
 		/// <summary>
--- ConsoleApp/testsql.sql
+++ ConsoleApp/testsql.sql
@@ -1,5 +1,9 @@
---simplest select
-select col1, [select+from] from tab1 where col1 = 5
+--ad hoc test
 
---all select clauses
-select col1, col2 into tab1 from tab2 where col1 = 5 and col2 = 1 group by col1, col2 having count(*) > 1 order by col3
+
+--simplest select
+select col1 from tab1 where col1 = 5;select col1 from tab1 where col1 = 5
+go
+
+--all select clauses, lists
+with tab2 as (select * from site) select t2.col1, col2, col1+col2 as col3 into tab1 from tab2 t2 join tab3 t3 on t3.ID = t2.ID where col1 = 5 and (col2 = 1 or col2 = 3) group by col1, col2, col1+col2 having count(*) > 1 order by col1, /*mlc*/ col2, col3
