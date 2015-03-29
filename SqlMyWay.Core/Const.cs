133560343b4257de20504214326bc2962a7353e4
adeelsyed <adeel.syed@gmail.com>
2015-03-28 23:42:08 -0700

    Don't know why consts.cs was not committed

---
+++ SqlMyWay.Core/Const.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlMyWay.Core
+{
+    static class Const
+    {
+        public static string[] ClauseKeywords = new string[]
+        {
+            "WITH",
+            "SELECT",
+            "INTO",
+            "FROM",
+            "JOIN",
+            "INNER JOIN",
+            "LEFT JOIN",
+            "LEFT OUTER JOIN",
+            "RIGHT OUTER JOIN",
+            "FULL OUTER JOIN",
+            "OUTER JOIN",
+            "WHERE",
+            "GROUP BY",
+            "HAVING",
+            "ORDER BY",
+            "UNION",
+            "INSERT",
+            "INSERT INTO",
+            "DELETE",
+            "DROP",
+            "UPDATE"
+        };
+    }
+}
